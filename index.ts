92d48d260bfe1b742b8c24ac71817a0290c11860
sankrkall2 <gowrisankarkalla4@gmail.com>
2024-10-15 16:22:27 +0530

    jfajfa

--- index.ts
+++ index.ts
@@ -1,3 +1,13 @@
 export function sum(a: number, b: number) {
   return a + b;
 }
+
+export function subtract(a: number, b: number) {
+  return a - b;
+}
+
+export function multiply(a: number, b: number) {
+  return a * b;
+}
+
+
